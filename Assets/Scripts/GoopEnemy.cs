e2e08d6ceddddbc789c396d2947916b2b4203516
Dawson Fields <dawson@gofields.com>
2021-01-29 22:13:22 -0600

    Add GoopEnemy & Flashlight

---
+++ Assets/Scripts/GoopEnemy.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GoopEnemy : MonoBehaviour
+{
+	[HideInInspector] public GameObject player;
+	public bool inLight = false;
+	public Rigidbody2D rb;
+
+	public float jumpForce = 5f;
+
+	private float distanceToGround;
+
+	private bool isGrounded;
+
+	public float currLightTimer = 0;
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		distanceToGround = GetComponent<Collider2D>().bounds.extents.y;
+	}
+
+
+	void Update()
+	{
+
+		if (inLight)
+		{
+			if (isGrounded)
+			{
+				rb.velocity = new Vector2(rb.velocity.x, 0);
+				rb.AddForce(((Vector2)(-1 * (player.transform.position - transform.position).normalized) + (Vector2.up)) * jumpForce);
+			}
+			currLightTimer -= Time.deltaTime;
+		}
+		else
+		{
+			if (isGrounded)
+			{
+				rb.velocity = new Vector2(rb.velocity.x, 0);
+				rb.AddForce(((Vector2)((player.transform.position - transform.position).normalized) + (Vector2.up)) * jumpForce);
+			}
+		}
+
+		if(currLightTimer < 0){
+			inLight = false;
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D c){
+		if(LayerMask.LayerToName(c.gameObject.layer) == "Platforms"){
+			isGrounded = true;
+		}
+	}
+	void OnCollisionExit2D(Collision2D c){
+		if(LayerMask.LayerToName(c.gameObject.layer) == "Platforms"){
+			isGrounded = false;
+		}
+	}
+}
