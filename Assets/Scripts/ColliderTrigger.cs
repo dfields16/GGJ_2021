340d8864549fb86130d46fb72a1d489ca1159d2a
Lenus Jack <Lenusjack4@yahoo.com>
2021-01-31 06:36:46 -0600

    Complete Opening Cutscene

--- Assets/Scripts/ColliderTrigger.cs
+++ Assets/Scripts/ColliderTrigger.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ColliderTrigger : MonoBehaviour
 {
     [SerializeField] Collider2D tilemapCollider;
+    [SerializeField] Orb orb;
+    [SerializeField] PlayerMovement movementScript;
 
     // Start is called before the first frame update
     void Start()
     {
         if (!tilemapCollider) tilemapCollider= FindObjectOfType<Collider2D>();
+        if (!orb) orb = FindObjectOfType<Orb>();
+        if (!movementScript) movementScript = FindObjectOfType<PlayerMovement>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,7 +21,15 @@ public class ColliderTrigger : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             tilemapCollider.enabled = true;
+            orb.TriggerCinematicGrow();
+            StartCoroutine(StopMovement());
         }
+    }
+
+    IEnumerator StopMovement()
+    {
+        yield return new WaitForSeconds(0.5f);
+        movementScript.enabled = false;
     }
 }
 
