a9ac320b9c4f1e7bf6ca855f61cb0c618d02d556
Lenus Jack <Lenusjack4@yahoo.com>
2021-01-30 18:22:04 -0600

    Add PlayerHealth Script

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] float maxHealth = 100f;
+    [SerializeField] float currentHealth;
+    Light playerLight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerLight = GetComponentInChildren<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerLight.intensity = currentHealth / 100;
+    }
+
+    public void PickupHealth(float health)
+    {
+        currentHealth += health;
+        if (currentHealth > maxHealth) { currentHealth = maxHealth; }
+    }
+
+    public void LoseHealth(float health)
+    {
+        currentHealth -= health;
+        if (currentHealth <= 0) { /*Trigger end game*/ }
     }
 }
