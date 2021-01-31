70cad5f4afc2beeaad8b9c383bf36bb52ad3853c
teatep <talia_tep@yahoo.com>
2021-01-31 12:25:30 -0600

    Added win area

---
+++ Assets/WinBox.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinBox : MonoBehaviour
+{
+
+	public GameMenu gameMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D){
+        // GameObject.Instantiate(gameMenu.gameObject, Vector3.zero, Quaternion.identity);
+        // GameMenu.menu.isPlayerDead(false);
+
+        SceneManager.LoadScene("Level1");
+
+    }
+}
