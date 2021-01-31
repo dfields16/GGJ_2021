1f05617e372c8401cc43816bd56f161ffeb07e6d
Lenus Jack <Lenusjack4@yahoo.com>
2021-01-30 22:07:33 -0600

    Fix player health/light interaction

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float maxHealth = 100f;
     [SerializeField] float currentHealth;
-    Light playerLight;
+    [SerializeField] Light2D playerLight;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        playerLight = GetComponentInChildren<Light>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         playerLight.intensity = currentHealth / 100;
+
+        if (Input.GetKeyDown(KeyCode.R)) 
+        { 
+            LoseHealth(25);
+            Debug.Log(currentHealth);
+        }
     }
 
     public void PickupHealth(float health)
